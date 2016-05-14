2abd362492a0da431d8a335c8bccdbbfa372edc4
gatesyp <ig11@zips.uakron.edu>
2016-05-14 15:18:39 -0400

    cleaned master brbanch

--- Example-HTTP/our-request.py
+++
@@ -1,9 +0,0 @@
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
-
-url = 'stoh.io/api/testing' # Set destination URL here
-post_fields = {'foo': 'bar'}     # Set POST fields here
-
-request = Request(url, urlencode(post_fields).encode())
-json = urlopen(request).read().decode()
-print(json)
--- Example-HTTP/sample-request.py
+++
@@ -1,9 +0,0 @@
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
-
-url = 'https://stoh.io/api/testing' # Set destination URL here
-post_fields = {'time': '130498234'}     # Set POST fields here
-
-request = Request(url, urlencode(post_fields).encode())
-json = urlopen(request).read().decode()
-print(json)
